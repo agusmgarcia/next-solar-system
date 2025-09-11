5f9e9e01f49839ae10ccb4e6dc2809d0dca22f1f
Agus Garcia <agusss.garcia19@gmail.com>
2025-09-11 11:58:00 -0300

    chore: bump dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component }: AppProps<any>) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link
-          href={`${process.env.NEXT_PUBLIC_BASE_PATH || ""}/favicon.ico`}
+          href={`${process.env.APP_BASE_PATH || ""}/favicon.ico`}
           rel="icon"
           type="image/x-icon"
         />
@@ -19,7 +19,7 @@ export default function App({ Component }: AppProps<any>) {
         <Component />
 
         <p className="absolute right-2 bottom-2 bg-transparent font-sans text-base text-white">
-          v{process.env.NEXT_PUBLIC_APP_VERSION}
+          v{process.env.APP_VERSION}
         </p>
       </div>
     </>
